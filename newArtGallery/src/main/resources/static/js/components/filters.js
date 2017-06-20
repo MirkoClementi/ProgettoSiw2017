cda75da1a7e3b27eb96a37fad725f0419d0cc344
Simone Fioroni <simonefioroni93@gmail.com>
2017-06-20 18:08:41 +0200

    final version

--- newArtGallery/src/main/resources/static/js/components/filters.js
+++ newArtGallery/src/main/resources/static/js/components/filters.js
@@ -1,16 +1,29 @@
 function myFunction() {
     var input, filter, ul, li, a, i;
-    input = document.getElementById("name-filter");
+    
+    
+    input = document.getElementById("nameFilter");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("paintings");
-    li = ul.getElementsByClassName("pics");
+    li = document.getElementsByClassName("pics");
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByClassName("pics")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    	if(li[i].id.toUpperCase().indexOf(filter)> -1 ){
+    		li[i].style.display ="";
+    	}else {
+            li[i].style.display = "none";
+        }
+    }
+
+   /* input = document.getElementById("name-filter"); //prendo input
+    filter = input.value.toUpperCase();  //UPCASE
+    ul = document.getElementsById("paintingContainer");  //prendo i figli di PC
+    li = ul.getElementsByClassName("pics");   //filtro PC e prendo solo le immagini
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByClassName("pics")[0]; //prendo la singola immagine
+        if (a.id.toUpperCase().indexOf(filter) > -1) { //il controllo va fatto su painting.artist.name
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         }
-    }
+    }*/
 }
 
